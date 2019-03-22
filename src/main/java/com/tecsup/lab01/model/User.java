846bed0d881b08db8d44d7ef1e2a52a04c94a3fc
yenifer <yenifer.santiago@tecsup.edu.pe>
2019-03-21 23:06:42 -0500

    cambio 2 User listo

--- src/main/java/com/tecsup/lab01/model/User.java
+++ src/main/java/com/tecsup/lab01/model/User.java
@@ -6,5 +6,10 @@ public class User {
 	public String nombre;
 	public String apellido;
 	public String edad;
-
+	
+	   public User (String nombre, String apellido, String edad) {
+           this.nombre = nombre;
+           this.apellido = apellido;
+           this.edad = edad;
+       } 
 }
