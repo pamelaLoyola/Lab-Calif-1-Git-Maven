c085ff85731a7ebab371d88d704c9fc5d1576d50
Pamela-Loyola <kogan.03.10@gmail.com>
2019-03-21 23:39:46 -0500

    Clase UserControl listo

--- src/main/java/com/tecsup/lab01/controller/UserController.java
+++ src/main/java/com/tecsup/lab01/controller/UserController.java
@@ -2,6 +2,11 @@ package com.tecsup.lab01.controller;
 import com.tecsup.lab01.view.*;
 import com.tecsup.lab01.model.*;
 
-public class UserController  {
-	
+public class UserController {
+    
+    public String datos(String nom, String ape, String eda){
+        User u = new User(nom, ape, eda);
+    	String data="El nombre es "+u.nombre+" "+u.apellido+" con edad de "+u.edad+" años";
+    	return data;
+    }
 }
